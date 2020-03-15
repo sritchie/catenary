7ee808cecc5f6a5859e69b99ef87f08219d3038d
Sam Ritchie <samritchie@google.com>
2020-03-15 11:00:46 -0600

    checkpoint
    Change-Id: I18bbd77fec9308f817bf0b17e118e62b30c09150

--- catenary/main.py
+++ catenary/main.py
@@ -19,46 +19,46 @@ IDEAS:
 
 """
 
-import jax as j
-import jax.lax as l
-import jax.numpy as np
 import functools
 
-import numpy as onp
+import jax as j
+import jax.lax as jl
+import jax.numpy as np
+import jax.numpy.linalg as la
+from jax.config import config
+
+config.update('jax_enable_x64', True)
 
 
-def t_k_plus_3(k, g_recip, state):
-  """Takes an array of state and calculates just single term. I
+def t_k_plus_3(k, g_recip, xs):
+  """Takes an array of state and calculates just single term."""
 
-  """
-  top = k - 1
+  def term(i, acc):
+    return acc + (xs[i] * xs[k - 1 - i])
 
-  def cake(i, acc):
-    return acc + (state[i] * state[top - i])
+  sum_term = jl.fori_loop(0, k, term, 0.0)
 
-  sum_term = l.fori_loop(0, top + 1, cake, 0.0)
-
-  return g_recip * (sum_term - state[k + 1])
+  return g_recip * (sum_term - xs[k + 1])
 
 
-def correlators(g, state):
+def correlators(g, xs):
   """Populates the supplied vector with correlator values."""
-  k = state.shape[0]
+  k = xs.shape[0]
 
   if k <= 2:
-    return state
+    return xs
 
-  g_recip = l.reciprocal(g)
+  g_recip = jl.reciprocal(g)
 
   def run(i, xs):
     new_v = t_k_plus_3(i - 3, g_recip, xs)
     return j.ops.index_update(xs, i, new_v)
 
-  return l.fori_loop(3, k + 1, run, state)
+  return jl.fori_loop(3, k + 1, run, xs)
 
 
 def initial_state(t1, t2, max_idx):
-  s = np.zeros(max_idx)
+  s = np.zeros(max_idx, dtype=np.double)
   return j.ops.index_update(s, j.ops.index[:3], [1, t1, t2])
 
 
@@ -74,9 +74,10 @@ def single_matrix_correlators(g, t1, t2, max_idx):
   return correlators(g, state)
 
 
-def sliding_window(xs, window):
+def rolling_rows(xs, window):
+  """"""
   top = xs.shape[0] - window + 1
-  wide = l.map(functools.partial(np.roll, xs, axis=0), -np.arange(top))
+  wide = j.vmap(functools.partial(np.roll, xs, axis=0))(-np.arange(top))
   return wide[:, :window]
 
 
@@ -84,13 +85,34 @@ def sliding_window(xs, window):
 def correlator_matrix(g, t1, t2, n):
   """The jitted correlator matrix producer, so good!"""
   correlators = single_matrix_correlators(g, t1, t2, (2 * n) - 1)
-  return sliding_window(correlators, n)
+  return rolling_rows(correlators, n)
 
 
-def smallest_eigenvalue(g, t1, t2):
-  """This is the final fucntion..."""
-  m = correlator_matrix(g, t1, t2, 100)
-  return np.sum(m)
+@functools.partial(j.jit, static_argnums=3)
+def smallest_eigenvalue(g, t1, t2, n):
+  """This is the final fucntion...
+
+  this gets us a forward pass derivative, NICE
+
+
+  """
+
+  m = correlator_matrix(g, t1, t2, n)
+  mev = la.eigvalsh(m)[0]
+  return np.abs(mev)
+
+
+def cake(n):
+  # THIS gets it done!
+  return j.jacfwd(smallest_eigenvalue, argnums=(0, 2))(1.2, 0.0, 2.5, n)
+
+
+@functools.partial(j.jit, static_argnums=3)
+def update(g, t1, t2, n):
+  step_size = 0.0001
+  (dg, dt1, dt2) = j.jacfwd(smallest_eigenvalue, argnums=(0, 2))(g, t1, t2, n)
+  return [(w - step_size * dw, b - step_size * db)
+          for (w, b), (dw, db) in zip(params, grads)]
 
 
 # So I THINK I can do this:
@@ -111,8 +133,7 @@ def smallest_eigenvalue(g, t1, t2):
 
 
 def main():
-  a = np.zeros(shape=10,)
-  key = j.random.PRNGKey(0)
+  cake(100)
 
 
 if __name__ == '__main__':
--- tests/catenary/test_main.py
+++ tests/catenary/test_main.py
@@ -11,27 +11,3 @@ def small_lists():
   return st.lists(st.integers(min_value=-10000, max_value=10000),
                   min_size=1,
                   max_size=100)
-
-
-@given(small_lists())
-def test_ranged_fold_left(xs):
-
-  # this does not currently work with lists.
-  acc = np.array(xs)
-
-  def squared_map(acc, i):
-    return acc[i] * acc[i]
-
-  result = m.ranged_fold_left(acc, squared_map)
-
-  assert list(result) == list(map(lambda x: x * x, xs))
-
-
-@given(small_lists())
-def test_safe_map(xs):
-
-  def square(x):
-    return x * x
-
-  result = m.safe_map(square, np.array(xs))
-  assert list(result) == list(map(square, xs))
